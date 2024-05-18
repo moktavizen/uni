255f5b1bd5732ce3ac1c14d6ab158433d94e209f
moktavizen <moktavizzen@gmail.com>
2024-05-18 16:14:31 +0700

    change api for data mining

--- d3/pa/data-mining/get-ayah-list.sh
+++ d3/pa/data-mining/get-ayah-list.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
+# Use method 2 if this doesn't work
 # Fetch JSON data from the URL and filter using jq, then save it to a file
 # /quran-tafsir will get ayah and tafsir, /quran-ayah will get ayah only
-# THIS USED TO WORK, SUDDENDLY IT ONLY RETURNS {}
-# curl -s "https://web-api.qurankemenag.net/quran-tafsir" \
-#   | jq '[.data[] | {id, surah_id, juz_id: .juz, ayah_num: .ayah, arabic, translation, tafsir: .tafsir.wajiz}]' \
-#   > ayah-list.json
-
-# Create an empty JSON array
-echo "[" > ayah-list.json
-
-# Loop through each endpoint and append its content to the JSON file
-for i in {1..6236}; do
-    curl -s "https://web-api.qurankemenag.net/quran-tafsir/$i" \
-        | jq '.data | {id, surah_id, surah_name: .surah.transliteration, juz_id: .juz, ayah_num: .ayah, arabic, translation, tafsir: .tafsir.wajiz}' \
-        >> ayah-list.json
-
-    # Add a comma after each JSON object except for the last one
-    if [ $i -ne 6236 ]; then
-        echo "," >> ayah-list.json
-    fi
-done
-
-# Close the JSON array
-echo "]" >> ayah-list.json
+curl -s "https://web-api.qurankemenag.net/quran-tafsir?" \
+  | jq '[.data[] | {id, surah_id, juz_id: .juz, ayah_num: .ayah, arabic, translation, tafsir: .tafsir.wajiz}]' \
+  > ayah-list.json
+# Method 2
+# # Create an empty JSON array echo "[" > ayah-list.json
+#
+# # Loop through each endpoint and append its content to the JSON file
+# for i in {1..6236}; do
+#     curl -s "https://web-api.qurankemenag.net/quran-tafsir/$i" \
+#         | jq '.data | {id, surah_id, surah_name: .surah.transliteration, juz_id: .juz, ayah_num: .ayah, arabic, translation, tafsir: .tafsir.wajiz}' \
+#         >> ayah-list.json
+#
+#     # Add a comma after each JSON object except for the last one
+#     if [ $i -ne 6236 ]; then
+#         echo "," >> ayah-list.json
+#     fi
+# done
+#
+# # Close the JSON array
+# echo "]" >> ayah-list.json
--- d3/pa/data-mining/get-surah-list.sh
+++ d3/pa/data-mining/get-surah-list.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+# Use method 2 if this doesn't work
 # Fetch JSON data from the URL and filter using jq, then save it to a file
-# THIS USED TO WORK, SUDDENDLY IT ONLY RETURNS {}
-# curl -s "curl -s "https://web-api.qurankemenag.net/quran-surah" | jq '[.data[]'" | jq '[.data[] | {id, arabic, transliteration, translation, num_ayah, location}]' >surah-list.json
+curl -s "https://web-api.qurankemenag.net/quran-surah?" \
+  | jq '[.data[] | {id, arabic, transliteration, translation, num_ayah, location}]' \
+  > surah-list.json
 
-# 
-# Create an empty JSON array
-echo "[" > surah-list.json
-
-# Loop through each endpoint and append its content to the JSON file
-for i in {1..114}; do
-    curl -s "https://web-api.qurankemenag.net/quran-surah/$i" | jq '.data | {id, arabic, transliteration, translation, num_ayah, location}' >> surah-list.json
-    # Add a comma after each JSON object except for the last one
-    if [ $i -ne 114 ]; then
-        echo "," >> surah-list.json
-    fi
-done
-
-# Close the JSON array
-echo "]" >> surah-list.json
+# # Method 2
+# # Create an empty JSON array
+# echo "[" > surah-list.json
+#
+# # Loop through each endpoint and append its content to the JSON file
+# for i in {1..114}; do
+#     curl -s "https://web-api.qurankemenag.net/quran-surah/$i" | jq '.data | {id, arabic, transliteration, translation, num_ayah, location}' >> surah-list.json
+#     # Add a comma after each JSON object except for the last one
+#     if [ $i -ne 114 ]; then
+#         echo "," >> surah-list.json
+#     fi
+# done
+#
+# # Close the JSON array
+# echo "]" >> surah-list.json
