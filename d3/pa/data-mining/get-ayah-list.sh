fac69024ce9557c9ddeb65987bf37deb00024467
moktavizen <moktavizzen@gmail.com>
2024-05-24 18:56:08 +0700

    fix jq filter

--- d3/pa/data-mining/get-ayah-list.sh
+++ d3/pa/data-mining/get-ayah-list.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 # Use method 2 if this doesn't work
-# Fetch JSON data from the URL and filter using jq, then save it to a file
+# Fetch data from the API and process it using jq
 # /quran-tafsir will get ayah and tafsir, /quran-ayah will get ayah only
 curl -s "https://web-api.qurankemenag.net/quran-tafsir?" \
-  | jq '[.data[] | {id, surah_id, juz_id: .juz, ayah_num: .ayah, arabic, translation, tafsir: .tafsir.wajiz}]' \
+  | jq '[.data[] | {
+      id,
+      surah_id,
+      surah_name: .surah.transliteration,
+      juz_id: .juz,
+      ayah_num: .ayah,
+      arabic,
+      translation,
+      tafsir: .tafsir.wajiz
+    }]' \
+  | sed -E 's/[0-9]+\)//g' \
   > ayah-list.json
+
 # Method 2
 # # Create an empty JSON array echo "[" > ayah-list.json
 #
