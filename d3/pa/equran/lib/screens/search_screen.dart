df002b11f0e8617a3201a668959993d1ebcbd471
moktavizen <moktavizzen@gmail.com>
2024-07-15 06:36:19 +0700

    fix lag on search and add debounce

--- d3/pa/equran/lib/screens/search_screen.dart
+++ d3/pa/equran/lib/screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:equran/databases/database.dart';
@@ -6,6 +7,7 @@ import 'package:equran/widgets/ayah_list_tile.dart';
 import 'package:equran/widgets/custom_app_bar.dart';
 import 'package:equran/widgets/juz_list_tile.dart';
 import 'package:equran/widgets/surah_list_tile.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 import 'package:fuzzywuzzy/model/extracted_result.dart';
@@ -13,6 +15,37 @@ import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
+
+class _SearchParams {
+  const _SearchParams({
+    required this.input,
+    required this.data,
+  });
+
+  final String input;
+  final List<dynamic> data;
+}
+
+List<ExtractedResult<dynamic>> _searchCompute(_SearchParams params) {
+  final results = extractTop(
+    query: params.input,
+    choices: params.data,
+    getter: (data) {
+      if (data is Surah) {
+        return data.latin;
+      } else if (data is Juz) {
+        return 'Juz ${data.id}';
+      } else if (data is Ayah) {
+        return data.latin;
+      } else {
+        return '';
+      }
+    },
+    limit: 10,
+  );
+
+  return results;
+}
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({
@@ -28,6 +61,8 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   final TextEditingController _textController = TextEditingController();
   List<ExtractedResult<dynamic>> _searchResults = [];
+  bool _isSearching = false;
+  Timer? _debounce;
   final SpeechToText _speechToText = SpeechToText();
   String _textFromSpeech = '';
   double level = 0.0;
@@ -35,30 +70,27 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   void dispose() {
     _textController.dispose();
+    _debounce?.cancel();
     _speechToText.cancel();
     super.dispose();
   }
 
   void _searchFromInput(String input) {
-    final results = extractTop(
-      query: input,
-      choices: widget.data,
-      getter: (data) {
-        if (data is Surah) {
-          return data.latin;
-        } else if (data is Juz) {
-          return 'Juz ${data.id}';
-        } else if (data is Ayah) {
-          return data.latin;
-        } else {
-          return '';
-        }
-      },
-      limit: 10,
-    );
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(milliseconds: 300), () async {
+      setState(() {
+        _isSearching = true;
+      });
 
-    setState(() {
-      _searchResults = results;
+      final results = await compute(
+        _searchCompute,
+        _SearchParams(input: input, data: widget.data),
+      );
+
+      setState(() {
+        _searchResults = results;
+        _isSearching = false;
+      });
     });
   }
 
@@ -89,6 +121,40 @@ class _SearchScreenState extends State<SearchScreen> {
     setState(() {
       this.level = level;
     });
+  }
+
+  Widget _buildFullList() {
+    return ListView.builder(
+      padding: const EdgeInsets.only(left: 24, right: 24, bottom: 64),
+      itemBuilder: (context, index) {
+        final result = widget.data.elementAt(index);
+        return _buildListTile(result);
+      },
+      itemCount: widget.data.length,
+    );
+  }
+
+  Widget _buildSearchResults() {
+    return ListView.builder(
+      padding: const EdgeInsets.only(left: 24, right: 24, bottom: 64),
+      itemBuilder: (context, index) {
+        final result = _searchResults.elementAt(index).choice;
+        return _buildListTile(result);
+      },
+      itemCount: _searchResults.length,
+    );
+  }
+
+  Widget _buildListTile(dynamic result) {
+    if (result is Surah) {
+      return SurahListTile(surah: result);
+    } else if (result is Juz) {
+      return JuzListTile(juz: result);
+    } else if (result is Ayah) {
+      return AyahListTile(ayah: result);
+    } else {
+      return const SizedBox.shrink();
+    }
   }
 
   @override
@@ -123,42 +189,14 @@ class _SearchScreenState extends State<SearchScreen> {
       ),
       body: Builder(
         builder: (context) {
-          if (_textController.text.isEmpty) {
-            return ListView.builder(
-              padding: const EdgeInsets.only(left: 24, right: 24, bottom: 64),
-              itemBuilder: (context, index) {
-                final result = widget.data.elementAt(index);
-                if (result is Surah) {
-                  return SurahListTile(surah: result);
-                } else if (result is Juz) {
-                  return JuzListTile(juz: result);
-                } else if (result is Ayah) {
-                  return AyahListTile(ayah: result);
-                } else {
-                  return null;
-                }
-              },
-              itemCount: widget.data.length,
+          if (_isSearching) {
+            return const Center(
+              child: RepaintBoundary(child: CircularProgressIndicator()),
             );
-          } else if (_textController.text.isNotEmpty) {
-            return ListView.builder(
-              padding: const EdgeInsets.only(left: 24, right: 24, bottom: 64),
-              itemBuilder: (context, index) {
-                final result = _searchResults.elementAt(index).choice;
-                if (result is Surah) {
-                  return SurahListTile(surah: result);
-                } else if (result is Juz) {
-                  return JuzListTile(juz: result);
-                } else if (result is Ayah) {
-                  return AyahListTile(ayah: result);
-                } else {
-                  return null;
-                }
-              },
-              itemCount: _searchResults.length,
-            );
+          } else if (_textController.text.isEmpty) {
+            return _buildFullList();
           } else {
-            return const SizedBox.shrink();
+            return _buildSearchResults();
           }
         },
       ),
